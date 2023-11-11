40052e38e0942ae487fbb3d38ba786d4bd5b5864
Khoa Le Minh <p3t3r276@gmail.com>
2023-11-11 16:38:31 +0700

    Change background color

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -42,8 +42,7 @@ const Post: FC<pageProps> = ({ params }) => {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full font-mono text-sm lg:flex">
         <h2 className="text-4xl text-center mb-4">{post.title}</h2>
-        <div>
-          {post.content}
+        <div dangerouslySetInnerHTML={{ __html: post.content }}>
         </div>
         <div>
           Created By: {post.createdBy?.name} at {dateTransform(post.createdAt)} <br /> 
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,3 @@
-// import ClassicEditor from "ckeditor5-custom-build/build/ckeditor";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import React from "react";
