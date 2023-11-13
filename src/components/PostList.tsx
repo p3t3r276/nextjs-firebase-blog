98f64b1fd0165e0ed50aac406cdb73d021d7d8bc
Khoa Le Minh <khoa.le.minh@preciofishbone.se>
2023-11-13 23:37:26 +0700

    Fix styles

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -36,19 +36,19 @@ export const PostList = () => {
     <div className='group flex flex-col gap-2 mt-4'>
       {posts.map((post, id) => (
         <div key={id} className='grid grid-cols-6 items-center'>
-          <Link href={`/posts/${post.id}`} className='col-span-4 rounded-lg bg-slate-400 p-5 text-white my-2'>
+          <Link href={`/posts/${post.id}`} className='col-span-4 rounded-l-lg bg-slate-400 p-5 text-white my-2'>
             <h2>{post.title}</h2>
             <div>Author: {post.createdBy?.name}</div>
             <div>Edited by: {post.updatedBy?.name} {durationFromNow(post.updatedAt)}</div>
           </Link>
           <button
-            className='flex items-center justify-center rounded-lg bg-blue-500 hover:bg-blue-400 text-white text-xl h-24'
+            className='flex items-center justify-center bg-blue-500 hover:bg-blue-400 text-white text-xl p-10'
             onClick={() => router.push(`/posts/edit/${post.id}`)}>
               <AiTwotoneEdit />
               <span className='sr-only'>Edit</span> 
             </button>
           <button
-            className='flex items-center justify-center rounded-lg bg-red-500 hover:bg-red-400 text-white text-xl h-24'
+            className='flex items-center justify-center rounded-r-lg bg-red-500 hover:bg-red-400 text-white text-xl p-10'
             onClick={() => removePost(post.id)}>
               
               <FaTrashAlt />
