563dd513dbf54b1cc9b7ee1118921358e7823614
Khoa Le Minh <p3t3r276@gmail.com>
2023-11-15 01:23:05 +0700

    Move authentication data to private file

--- src/db/firebase.ts
+++ src/db/firebase.ts
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB43Pk0ZqTPZw9dScVOt8mWngM6KdwyA0c",
-  authDomain: "blog-with-tags.firebaseapp.com",
-  projectId: "blog-with-tags",
-  storageBucket: "blog-with-tags.appspot.com",
-  messagingSenderId: "382914189767",
-  appId: "1:382914189767:web:336865ffbac506c2c121e2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
