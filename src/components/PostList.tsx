c80ee6e2248da3fe0666cfaafc89a32c928aa76c
Khoa Le Minh <p3t3r276@gmail.com>
2023-12-11 14:56:00 +0700

    Delete post and all its tag subcollection

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation'
-import { collection, deleteDoc, doc, getDocs, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { Firestore, Query, collection, deleteDoc, doc, getDocs, limit, onSnapshot, orderBy, query, writeBatch } from 'firebase/firestore';
 import { AiTwotoneEdit } from 'react-icons/ai'
 import { FaTrashAlt } from 'react-icons/fa'
 
@@ -30,27 +30,26 @@ export const PostList = () => {
 
   const removePost = async (postId: string) => {
     // delete subcollection from web client is not recommended
-    // await deleteDoc(doc(db, postCollection, postId))
-    await fetch('/api/posts', {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ postId }),
-    })
-    .then(response => {
-      console.log(response)
-    })
-    .catch(error => {
-      console.log(error)
-    });
-    // await fetch('/api/posts')
+    // setup API endpoint
+    // await fetch('/api/posts', {
+    //   method: 'DELETE',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({ postId }),
+    // })
     // .then(response => {
     //   console.log(response)
     // })
     // .catch(error => {
     //   console.log(error)
     // });
+    const postDocRef = doc(db, postCollection, postId)
+    const tagSubColPath: string = `${postCollection}/${postId}/${tagCollection}`;
+
+    // delete all tags first
+    await deleteCollection(db, tagSubColPath)
+    await deleteDoc(postDocRef)
   }
 
   return (
@@ -79,4 +78,37 @@ export const PostList = () => {
       ))}
     </div>
   )
+}
+
+async function deleteCollection(db: Firestore, collectionPath: string, batchSize: number = 20) {
+  const collectionRef = collection(db, collectionPath);
+  const collectionQuery = query(collectionRef, limit(batchSize));
+
+  return new Promise((resolve, reject) => {
+    deleteQueryBatch(db, collectionQuery, resolve).catch(reject);
+  });
+}
+
+async function deleteQueryBatch(db: Firestore, query: Query, resolve: (value: unknown) => void) {
+  const snapshot = await getDocs(query);
+
+  const batchSize = snapshot.size;
+  if (batchSize === 0) {
+    // When there are no documents left, we are done
+    resolve(true);
+    return;
+  }
+
+  // Delete documents in a batch
+  const batch = writeBatch(db);
+  snapshot.docs.forEach((doc) => {
+    batch.delete(doc.ref);
+  });
+  await batch.commit();
+
+  // Recurse on the next process tick, to avoid
+  // exploding the stack.
+  process.nextTick(() => {
+    deleteQueryBatch(db, query, resolve);
+  });
 }
